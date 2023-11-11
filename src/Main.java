2576009b3a7d9121f4c799f3baeae6c94929ce55
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-11-11 13:47:47 -0300

    upload files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        IssueBikeUI ui = new IssueBikeUI();
+
+        ui.showBikeDetails(100);
+        ui.calculateCost(5);
+        ui.createCustomer("Les Hergraves", "PW2 6TR", 1462501339);
+        ui.calculateTotalPayment();
+    }
+}
